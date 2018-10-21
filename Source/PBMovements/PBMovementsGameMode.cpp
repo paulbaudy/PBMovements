b36b1a0a6c968f05c7db927f400dd595c80dd246
Paul Baudy <paulbaudy@users.noreply.github.com>
2018-10-21 17:39:39 +0200

    Added HUD

--- Source/PBMovements/PBMovementsGameMode.cpp
+++ Source/PBMovements/PBMovementsGameMode.cpp
@@ -3,13 +3,19 @@
 #include "PBMovementsGameMode.h"
 #include "PBMovementsCharacter.h"
 #include "UObject/ConstructorHelpers.h"
-
+#include "Runtime/Engine/Classes/GameFramework/HUD.h"
 APBMovementsGameMode::APBMovementsGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<AHUD> HudBPClass(TEXT("/Game/HUD/PBHUD"));
+
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+	if (HudBPClass.Class != NULL)
+	{
+		HUDClass = HudBPClass.Class;
 	}
 }
